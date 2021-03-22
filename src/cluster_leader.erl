ee35184cb731e36e8917e74d0d22ad52737ed67a
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2021-03-22 10:19:12 +0100

    leader conflict resolution based on uptime

--- src/cluster_leader.erl
+++ src/cluster_leader.erl
@@ -4,7 +4,7 @@
 
 -export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2, handle_continue/2, terminate/2,
          code_change/3]).
--export([attempt_leader/0]).
+-export([attempt_leader/0, uptime/0]).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -40,7 +40,7 @@ attempt_leader() ->
     attempt_leader(cluster:is_leader()).
 
 attempt_leader(false) ->
-    case global:register_name(cluster_leader, self()) of
+    case global:register_name(cluster_leader, self(), fun resolve/3) of
         yes ->
             lager:notice("CLUSTER new leader is ~p", [node()]),
             cluster:notify_observers({cluster, leader_changed});
@@ -51,3 +51,26 @@ attempt_leader(false) ->
     ok;
 attempt_leader(true) ->
     ok.
+
+uptime() ->
+    erlang:monotonic_time() - erlang:system_info(start_time).
+
+resolve(_, Pid1, Pid2) ->
+    Node1 = node(Pid1),
+    Node2 = node(Pid2),
+    Up1 = rpc:call(Node1, ?MODULE, uptime, []),
+    Up2 = rpc:call(Node2, ?MODULE, uptime, []),
+    Winner = resolve(Pid1, Up1, Pid2, Up2),
+    lager:notice("CLUSTER name conflict winner ~p", [node(Winner)]),
+    Winner.
+
+resolve(Pid1, Up1, Pid2, Up2) when is_integer(Up1) andalso is_integer(Up2) ->
+    case Up1 > Up2 of 
+        true ->
+            Pid1;
+        false ->
+            Pid2
+    end;
+
+resolve(Pid1, _, _, _) -> Pid1.
+
